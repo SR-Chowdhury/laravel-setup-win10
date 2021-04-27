3d2c8c94cf2f375bfbb5e289edf4876beeb85e80
SR-Chowdhury <hackedgdown@gmail.com>
2021-04-27 12:25:18 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    </head>
+    <body class="antialiased">
+        <div class="container mt-5">
+            <h1 class="text-center">Bismillahir Rahmanir Rahim</h1>
+            <h2 class="text-center">Laravel & XAMPP setup in Windows 10 Tutorial</h2>
+        </div>
+    </body>
+</html>
